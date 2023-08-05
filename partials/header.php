c9c1a5c981144c596a211215963011281d05b03b
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-04 22:20:52 -0600

    Welcome page para usuarios no registrados

--- partials/header.php
+++ partials/header.php
@@ -8,6 +8,10 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.1/darkly/bootstrap.min.css" integrity="sha512-TW3EkvQFl2hehLxEL4TSEmP960SIWeTAdT6MWTdhbRdRyBRmQjEiWpoDW5VpgZQRy5RJE2qhugkeISF3BtDd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./static/css/index.css" />
+  <?php $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) ?>
+  <?php if ($uri == "/contacts-app/" || $uri == "/contacts-app/index.php"): ?>
+    <script defer src="./static/js/welcome.js"></script>
+  <?php endif ?>
   <title>Contacts App</title>
 </head>
 
