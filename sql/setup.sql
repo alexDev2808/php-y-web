2b700870c3556459ba8e7154ad23df6b068a8216
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-05 18:34:26 -0600

    Tabla usuarios y formulario de registro

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,14 @@
 CREATE DATABASE contacts_app;
 USE contacts_app;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
+
+INSERT INTO users (name, email, password) VALUES ("alexis", "test@test.com", "12345");
+
 CREATE TABLE contacts(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
